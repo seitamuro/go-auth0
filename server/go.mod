905c4c9f50fa1237a020f524797d27ba647a65e0
seitamuro <seitamuro0210@gmail.com>
2023-02-01 11:56:35 +0900

    add environemnt variables

--- server/go.mod
+++ server/go.mod
@@ -5,5 +5,6 @@ go 1.19
 require (
 	github.com/auth0/go-jwt-middleware v1.0.1 // indirect
 	github.com/form3tech-oss/jwt-go v3.2.2+incompatible // indirect
+	github.com/joho/godotenv v1.4.0 // indirect
 	github.com/rs/cors v1.8.3 // indirect
 )
--- server/main.go
+++ server/main.go
@@ -4,27 +4,52 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
+	"os"
 
+	"github.com/joho/godotenv"
+	"github.com/rs/cors"
 	v1 "github.com/seitamuro/go-auth0/server/handlers/v1"
-)
-
-const (
-	port = 8000
+	"github.com/seitamuro/go-auth0/server/handlers/v1/users/me"
+	"github.com/seitamuro/go-auth0/server/middlewares/auth0"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	url := os.Getenv("API_URL")
+	domain := os.Getenv("AUTH0_DOMAIN")
+	clientID := os.Getenv("AUTH0_CLIENT_ID")
+
+	jwks, err := auth0.FetchJWKS(domain)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	jwtMiddleware, err := auth0.NewMiddleware(domain, clientID, jwks)
+	if err != nil {
+		log.Fatal(err)
+	}
 	mux := http.NewServeMux()
-	// /v1へのリクエストが来た場合のハンドラを追加
 	mux.HandleFunc("/v1", v1.HandleIndex)
+	mux.Handle("/v1/users/me", auth0.UseJWT(http.HandlerFunc(me.HandleIndex)))
 
-	addr := fmt.Sprintf(":%d", port)
+	c := cors.New(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:3000"},
+		AllowedHeaders:   []string{"Authorization", "Content-Type"},
+		AllowCredentials: true,
+		Debug:            true,
+	})
 
-	// localhost:8000 でサーバーを立ち上げる
-	log.Printf("Listening on %s", addr)
-	if err := http.ListenAndServe(addr, mux); err != nil {
+	wrappedMux := auth0.WithJWTMiddleware(jwtMiddleware)(mux)
+	wrappedMux = c.Handler(wrappedMux)
+
+	log.Printf("Listing on %s", url)
+	if err := http.ListenAndServe(url, wrappedMux); err != nil {
 		log.Fatal(err)
 	}
 }
